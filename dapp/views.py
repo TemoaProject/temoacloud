41d0e8088f3dcdecedd90459c3f9ecca0c1f5956
Vivek Ojha <vivekojhaiit@gmail.com>
2016-05-21 20:53:58 +0530

    add template

--- dapp/views.py
+++ dapp/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+ 
 def index(request):
-	return HttpResponse("Index SSS")
+    return render_to_response('login.html', context_instance=RequestContext(request))
--- dproject/settings.py
+++ dproject/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'dapp'
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'dproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['/home/vivek/code/python/dapp/dapp/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,3 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
+STATIC_ROOT = "/home/vivek/code/dapp/static/"
+
+
