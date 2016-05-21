9de5cfe1fa0fd36acf33d70078f0b03a57d84529
Vivek Ojha <vivekojhaiit@gmail.com>
2016-05-21 22:41:09 +0530

    add changes

--- dapp/urls.py
+++ dapp/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     url(r'^login$', views.login, name='login'),
     url(r'^input$', views.inputData, name='input'),
     url(r'^output$', views.outputData, name='output'),
-    url(r'^modelrun$', views.modelRun, name='modelrun')
+    url(r'^modelrun$', views.modelRun, name='modelrun'),
     url(r'^model$', views.runModel, name='model')
 ]
 
